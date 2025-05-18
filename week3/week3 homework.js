7d2473020cebf2633e37ada025f7530e6879c412
INGRoyer <mitchellyroyer@gmail.com>
2025-05-18 15:45:17 -0500

    Homework week 3 without challenge

---
+++ week3/week3 homework.js
@@ -0,0 +1,66 @@
+//Array pf pizzaTopping
+let pizzaToppings =["cheese", "pinapple","pepperoni","onions","chicken"];
+
+//create a greetcustomer function 
+function greetcustomer(){
+    console.log("Welcome to Pizza Housse, our Toppings are:");
+    for (let topping of pizzaToppings){
+        console.log(topping +",");
+    }
+}  greetcustomer()
+
+//create a getPizzaOrder function
+let order = getPizzaOrder('large','Thick','pepperoni','omions')
+function getPizzaOrder(size, crust, ...toppings){
+  console.log(`one ${size} ${crust} crust pizza with ${toppings} coming up!`);
+  return[size, crust, ...toppings];  
+
+}
+  
+//create a pizzaprepare function
+let pizza=['medium', 'thick',['olives', 'grenn peppers', 'onions']];
+ let cookedpizza=preparePizza(pizza);
+function preparePizza([size, crust, toppings]){
+    console.log('....cooking pizza....');
+
+    const Object ={
+        size: size,
+        crust: crust,
+        toppings: toppings
+    };
+    return Object;
+}
+
+
+//create a servePizza function
+
+function servePizza(size, crust, ...toppings){
+    console.log(`order oup! here's your ${size} ${crust} crust pizza with ${toppings}. Enjoy!`);
+
+}
+
+ const mypizza  ={
+        size: 'large',
+        crust: 'thick',
+        toppings: ['pepperoni', 'onions', 'olives']
+    };
+
+
+
+
+console.log(order);
+
+servePizza("lagre", "thick", "pepperoni, onions")
+
+
+
+
+//bonus challenge
+
+
+
+
+
+ 
+
+    
